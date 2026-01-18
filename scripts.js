5add77522039db84fbac6b8ae537fbdafda1ec47
renansiebert7 <renansiebert01@gmail.com>
2026-01-18 00:09:55 -0300

    commit alterações

--- scripts.js
+++ scripts.js
@@ -29,6 +29,38 @@ document.getElementById("btnCurriculo").addEventListener("click", function () {
     link.click();
 });
 
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu-mobile");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
+
+document.querySelectorAll(".nav-menu button").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
+
+window.addEventListener("click", (e) => {
+    if (navMenu.classList.contains("active") && 
+        !navMenu.contains(e.target) && 
+        !hamburger.contains(e.target)) {
+        
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+});
+
 const btnTopo = document.querySelector(".topo")
 const footer = document.querySelector(".contact-links");
 
